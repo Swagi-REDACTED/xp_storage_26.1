0c6676bf2c883960ca666eb03825b961612eab57
Swagi-REDACTED <111149273+Swagi-REDACTED@users.noreply.github.com>
2026-05-04 22:37:50 -0400

    Delete src/main directory

--- src/main/java/com/github/charlyb01/xpstorage/config/CosmeticConfig.java
+++
@@ -1,9 +0,0 @@
-package com.github.charlyb01.xpstorage.config;
-
-import me.fzzyhmstrs.fzzy_config.config.ConfigSection;
-
-public class CosmeticConfig extends ConfigSection {
-    public ExperienceTooltip bookTooltip = ExperienceTooltip.LEVEL;
-    public ExperienceTooltip bottleTooltip = ExperienceTooltip.LEVEL;
-    public int glint = 100;
-}
--- src/main/java/com/github/charlyb01/xpstorage/config/ExperienceTooltip.java
+++
@@ -1,7 +0,0 @@
-package com.github.charlyb01.xpstorage.config;
-
-public enum ExperienceTooltip {
-    LEVEL,
-    POINT,
-    LEVEL_POINT
-}
